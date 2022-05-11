5f1ac26d37e9a559e322b515d61339aded736b76
Ebube111 <ebubeagwaze@gmail.com>
2022-05-11 07:39:53 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,92 +1,61 @@
 #!/usr/bin/python3
-''' Python3 program to solve N Queen '''
+""" N queens backtracking """
 import sys
 
 
+class NQueen:
+    """ Class Queens """
+
+    def __init__(self, n):
+        """ Constructor """
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
+
+    def place(self, k, i):
+        """ Check if a secure place
+        """
+
+        for j in range(1, k):
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """ Resolve the nqueen
+        """
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                self.x[k] = i
+                if k == self.n:
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    self.nQueen(k + 1)
+        return self.res
+
+
 if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
+    print("Usage: nqueens N")
+    sys.exit(1)
+
 N = sys.argv[1]
+
 try:
     N = int(N)
-
-except:
-    print('N must be a number')
-    exit(1)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
 if N < 4:
-    print('N must be at least 4')
-    exit(1)
+    print("N must be at least 4")
+    sys.exit(1)
 
-k = 1
+queen = NQueen(N)
+result = queen.nQueen(1)
 
-
-def printSolution(board):
-<<<<<<< HEAD
-""" A utility function to print solution """
-=======
-    """ A utility function to print solution """
->>>>>>> edf7d1035eaf240a5f9ea7e096db12608fb50a56
-    queens = []
-    global k
-    k = k + 1
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 1:
-                queens.append([i, j])
-    print(queens)
-
-
-def isSafe(board, row, col):
-    for i in range(col):
-        if board[row][i]:
-            return False
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if board[i][j]:
-            return False
-        i -= 1
-        j -= 1
-    i = row
-    j = col
-    while j >= 0 and i < N:
-        if board[i][j]:
-            return False
-        i = i + 1
-        j = j - 1
-    return True
-
-
-def solveNQUtil(board, col):
-<<<<<<< HEAD
-""" This function solves the N Queen problem """
-=======
-    """ This function solves the N Queen problem """
->>>>>>> edf7d1035eaf240a5f9ea7e096db12608fb50a56
-    if col == N:
-        printSolution(board)
-        return True
-    res = False
-    for i in range(N):
-        if isSafe(board, i, col):
-            board[i][col] = 1
-            res = solveNQUtil(board, col + 1) or res
-            board[i][col] = 0
-    return res
-
-
-def solveNQ():
-<<<<<<< HEAD
-""" solve NQ """
-=======
-    """ solve NQ """
->>>>>>> edf7d1035eaf240a5f9ea7e096db12608fb50a56
-    board = [[0 for j in range(N)] for i in range(N)]
-    if solveNQUtil(board, 0) is False:
-        pass
-        return
-    return
-
-
-solveNQ()
+for i in result:
+    print(i)
